d1f7a463c3683eb12e82cd83a76635c4dca558f4
TRAN Quoc Hung <quochungtr99@gmail.com>
2025-06-20 00:04:26 +0200

    Implement stack interface of lib datastructures

--- data_structures/src/stack.hpp
+++ data_structures/src/stack.hpp
@@ -1,10 +1,24 @@
 #ifndef DATA_STRUCTURES_STACK_HPP_
 #define DATA_STRUCTURES_STACK_HPP_
 
-#include "node.hpp"
-#include <stdexcept>  /// for std::invalid_argument
+#include <iostream>
 
 namespace data_structure {
 
+template<typename T>
+class Stack{
+    virtual std::size_t getSize() const                       = 0;
+    virtual bool full() const                                 = 0;
+    virtual bool isEmpty() const                              = 0;
+    virtual void push(const T& iValue)                        = 0;
+    virtual void push(const T&& iValue)                       = 0;
+    virtual T pop() const                                     = 0;
+    virtual std::ostream& toStream(std::ostream& ioOut) const = 0;
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& ioOut, const Stack<T>& iStack){
+    return iStack.toStream();
+}
 }
 #endif  // DATA_STRUCTURES_STACK_HPP_
