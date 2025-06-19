ccc744b23e5e42f8caf40bfd1c09ebc29616b23b
TRAN Quoc Hung <quochungtr99@gmail.com>
2025-06-19 23:29:29 +0200

    clean up

--- data_structures/src/stack.hpp
+++ data_structures/src/stack.hpp
@@ -6,57 +6,5 @@
 
 namespace data_structure {
 
-template <class ValueType>
-class stackK {
-public:
-    using value_type = ValueType;
-
-    bool isEmptyStack() const {return (stackTop == nullptr);}
-
-    void push(const value_type& item){
-        auto newNode = std::make_shared<Node<value_type>>(item, stackTop);
-        stackTop = newNode;
-        _size++;
-    }
-
-    std::size_t getSize() const{
-        return _size;
-    }
-
-    void pop() {
-        ensureNotEmpty();
-        stackTop = stackTop->next;
-        _size--;
-    }
-    
-    value_type top() const{
-        ensureNotEmpty();
-        return stackTop->data;
-    }
-
-    void clear() {
-        stackTop = nullptr;
-        _size = 0;
-    }
-
-    void display() const {
-        std::cout << "Top --> ";
-        display_all(this->stackTop.get());
-        std::cout << "\n";
-        std::cout << "Size of stack" << _size << std::endl; 
-    }
-
-private:
-    void ensureNotEmpty() const {
-        if (isEmptyStack()){
-            throw std::invalid_argument("Stack is empty.");
-        }
-    }
-
-private:
-    std::shared_ptr<Node<value_type>> stackTop = {};
-    std::size_t _size = 0;
-};
-
 }
 #endif  // DATA_STRUCTURES_STACK_HPP_
