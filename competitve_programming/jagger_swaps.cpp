e7afde2e82d62109519cf42c8080e5669cb03ef2
TRAN Quoc Hung <quochungtr99@gmail.com>
2025-11-04 22:03:43 +0100

    jagger swaps

---
+++ competitve_programming/jagger_swaps.cpp
@@ -0,0 +1,18 @@
+#include "template.hpp"
+
+void solve() {
+    ll n;
+    in(n);
+    vector<ll> a(n);
+    in(a);
+    
+    if (a[0] == 1){
+        print("YES");
+    }
+    else
+    {
+        print("NO");
+    }
+
+    return;
+}
