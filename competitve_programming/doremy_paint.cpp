068464697e9dafbfee579112ae042264585a57f3
TRAN Quoc Hung <quochungtr99@gmail.com>
2025-11-04 22:24:25 +0100

    2 CPs

---
+++ competitve_programming/doremy_paint.cpp
@@ -0,0 +1,34 @@
+#include "template.hpp"
+
+void solve() {
+    int n;
+    in(n);
+    vector<int> a(n);
+    in(a);
+    map<int, int> mp;
+
+    for (int i = 0; i < n; i++){
+        mp[a[i]] += 1;
+    }
+
+    if (mp.size() == 1){
+        print("YES");
+    }
+    else if (mp.size() == 2){
+        auto firstCount  = mp.begin()->second;
+        auto secondCount = mp.rbegin()->second;
+        if (firstCount == secondCount){
+            print("YES");
+        } 
+        else if (n%2 == 1 && abs(firstCount - secondCount) == 1){
+            print("YES");
+        }
+        else{
+            print("NO");
+        }
+    }
+    else{
+        print("NO");
+    }
+    return;
+}
